338990975188eb31fc1cce74cbc2bf4264757f32
ZhongXun <kkzxak47@gmail.com>
2016-08-13 13:41:05 +0800

    add range to int

---
+++ v2ex/range_to_int.py
@@ -0,0 +1,18 @@
+#/usr/bin/env python3
+# coding: utf-8
+
+import bisect
+
+def main():
+	arr = [1, 2, 3, 4]
+	d = {1:1, 2:2, 3:5}
+	idx = bisect.bisect_left(arr, 1.5)
+	print(d[idx])
+	idx = bisect.bisect_left(arr, 2.5)
+	print(d[idx])
+	idx = bisect.bisect_left(arr, 3.5)
+	print(d[idx])
+
+
+if __name__ == '__main__':
+	main()
